dd2ec0b4bae250bd2c9e5d0903f9861e50982a3d
Mahad Walusimbi <engmk123@gmail.com>
2017-06-13 01:38:12 +0300

    Pylint command

--- blog/models.py
+++ blog/models.py
@@ -35,6 +35,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self)
+    
         args = [
             self.publish.year, self.publish.strftime('%m'),
             self.publish.strftime('%d'), self.slug
